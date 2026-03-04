999fc2b29045ca20d934af70fe2152862e8eccea
talipan2 <osmanov1988figa@gmail.com>
2026-03-04 18:49:24 +0300

    fix bugs

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -76,7 +76,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false, // Отключаем на тач-устройствах
+      // smoothTouch: false, // Отключаем на тач-устройствах
     });
     lenisRef.current = lenis;
 
